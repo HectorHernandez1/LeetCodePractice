e711a826213ecccd55799cec256d8d2253974022
HectorHernandez1 <hhhector9@gmail.com>
2026-01-27 13:58:40 -0700

    finish the valid parentheses problem

--- easy/013_valid_parentheses.py
+++ easy/013_valid_parentheses.py
@@ -60,8 +60,24 @@ class Solution:
         Returns:
             bool: True if valid, False otherwise
         """
-        # Write your code here
-        pass
+        grounded_truth = {")": "(", "}": "{", "]": "["}
+        right_brackets = ")}]"
+        stack = []
+        for i in list(s):
+            if i not in right_brackets:
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                left_bracket = stack.pop()
+                expected = grounded_truth[i]
+                if left_bracket != expected:
+                    return False
+        if len(stack)==0:
+            return True
+        else:
+            return False
+
 
 
 # Test cases
